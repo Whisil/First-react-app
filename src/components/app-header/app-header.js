b2b54a8f193f1afad93be08b9d7a2189ff75489d
David <electromandarin@gmail.com>
2021-09-17 21:21:32 +0300

    first commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="d-flex app-header">
+            <h1>David Haidamaka</h1>
+            <h2>5 записей, из них понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import AppHeader from '../app-header/app-header';
+import SearchPanel from '../search-panel/search-panel';
+import PostStatusFilter from '../post-status-filter/post-status-filter';
+import PostList from '../post-list/post-list';
+import PostAddForm from '../post-add-form/post-add-form';
+
+import './app.css';
+
+const App = () =>{
+
+    const data = [
+        {label: 'Going to learn React', important: true, id:"asdw"},
+        {label: 'That is so good', important: false, id:"adsf"},
+        {label: 'I need a break...', important: false, id:"asdas"}
+    ];
+
+    return(
+        <div className="app">
+            <AppHeader />
+            <div className="seacrh-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList posts={data}/>
+            <PostAddForm/>
+        </div>
+        
+    )
+}
+
+export default App;
