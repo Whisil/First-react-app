417ad47270f46740d76332c542c2636d0656de90
David <electromandarin@gmail.com>
2021-09-24 19:45:34 +0300

    final commit

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,16 +3,35 @@ import './post-list.css';
 
 import PostListItem from '../post-list-item/post-list-item'
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map((item) => {
         const {id, ...itemProps} = item;
-        return (
-            <li key={id} className="list-group-item">
-                <PostListItem {...itemProps} />
-            </li>
-        )
+
+        function isEmpty(obj){
+            for (let k in obj) {
+                return true
+            }
+            return false;
+        }
+
+        if(typeof item == 'object' && isEmpty(item)){
+            return (
+                <li key={id} className="list-group-item">
+                    <PostListItem 
+                        {...itemProps}
+                        onDelete={() => onDelete(id)}
+                        onToggleImportant={() => onToggleImportant(id)}
+                        onToggleLiked={() => onToggleLiked(id)}/>
+                </li>
+            )
+        } else {
+            console.log('not an object');
+        }
+        
     });
+
+
 
     return (
         <ul className="app-list list-group">
